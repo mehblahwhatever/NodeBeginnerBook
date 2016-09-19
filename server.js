faa85c5f4df874b47556508c413bc2c5a709e1b6
Phillip <mehblahwhatever@gmail.com>
2016-09-19 09:22:15 -0400

    removed anonymous functions from server.js

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 var http = require("http");
 
-http.createServer(function (request, response) {
+function onRequest(request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello World");
 	response.end();
-}).listen(8888);
+}
 
-/*
-// Valid
-// This creates a server listening at port 8888, but does nothing else
-var http = require("http");
-
-var server= http.createServer();
-server.listen(8888);
-*/
+http.createServer(onRequest).listen(8888);
